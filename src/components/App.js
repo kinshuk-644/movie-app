034d938e2b0f627363e5d8266d9471e5afd1fb96
Kinshuk <kinshuktaya644@gmail.com>
2022-01-02 19:12:28 +0530

    Action types and action creators

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { data } from '../data';
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
   componentDidMount() {
@@ -15,10 +16,7 @@ class App extends React.Component {
     // make api call to get movies
 
     // dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
 
     console.log(store.getState());
   }
