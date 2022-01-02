0cd49aa40cb694aef06a673ae7c58a30ba397080
Kinshuk <kinshuktaya644@gmail.com>
2022-01-02 18:30:06 +0530

    Dispatching actions

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ import movies from './reducers';
 
 const store = createStore(movies);
 console.log(store);
-console.log(store.getState());
+console.log('Before State', store.getState());
+
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [{name: 'Superman'}]
+});
+
+console.log('After State', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
