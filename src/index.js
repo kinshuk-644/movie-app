c5e915727d7503c95417787fc617df8ebe75cbc3
Kinshuk <kinshuktaya644@gmail.com>
2022-01-08 12:30:31 +0530

    Modify middleware

--- src/index.js
+++ src/index.js
@@ -5,19 +5,26 @@ import { createStore, applyMiddleware } from 'redux';
 
 // file imports
 import './index.css';
-import App from './components/App'; 
+import App from './components/App';
 import rootReducer from './reducers';
 
 // function logger(obj, next, action)
 // logger(obj)(next)(action)
-const logger = function({ dispatch, getState }) {
-  return function(next) {
-    return function(action) {
-      // middleware code
-      console.log('ACTION_TYPE = ', action.type);
-      next(action);
-    }
-  }
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       // middleware code
+//       console.log('ACTION_TYPE = ', action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+// a simpler way to write above commented code
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  // middleware code
+  console.log('ACTION_TYPE = ', action.type);
+  next(action);
 }
 
 const store = createStore(rootReducer, applyMiddleware(logger));
@@ -33,7 +40,7 @@ console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App store={store}/>
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
